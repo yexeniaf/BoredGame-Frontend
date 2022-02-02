3ad028cf41edc743907114c58d8ae8ba4912417f
Kevin Cleary <kjc2274@yahoo.com>
2022-02-02 11:51:49 -0500

    made edit and login forms

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import  LandingPage from "./components/LandingPage";
 import './App.css';
 import Signup from './screens/Signup';
+import Edit from "./screens/Edit";
+import Login from "./screens/Login";
 
 function App() {
   return (
@@ -9,10 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route> Home Page</Route>
-        <Route path="/signup" element={<Signup/>}>Sign Up Page </Route>
-        <Route>Login Page</Route>
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login/>}/>
         <Route>Account Info Page</Route>
-        <Route>Edit Account Page</Route>
+        <Route path="/edit" element={<Edit/>}/>
         <Route> Game Page</Route>
         <Route> Saved Page</Route>
         <Route> Error Page</Route>
