b56b98b233f3a5a701b1c6813655ccb3cddd1461
Kevin Cleary <kjc2274@yahoo.com>
2022-02-02 16:27:30 -0500

    added more dice

--- client/src/components/Dice/RollDice.jsx
+++ client/src/components/Dice/RollDice.jsx
@@ -2,22 +2,28 @@ import { useEffect, useState } from "react";
 
 export default function RollDice() {
     const [die, setDie] = useState(1);
+    const [die2, setDie2] = useState(1);
+    const [die3, setDie3] = useState(1);
     const [toggle, setToggle] = useState(false);
 
-    const roll = ()=>{
+    const roll = () => {
         setToggle((prevToggle)=> !prevToggle)
     }
 
     useEffect(() => {
-        setDie(Math.floor(Math.random()*6 + 1))
-        console.log(die);
+        setDie(Math.floor(Math.random()*6 + 1));
+        setDie2(Math.floor(Math.random()*6 + 1));
+        setDie3(Math.floor(Math.random()*6 + 1))
     }, [toggle])
-
+    console.log(die);
+    
 
   return (
-    <div>
-        <button onClick={roll}>Roll Die</button>
+    <div className="m-5">
+        <button onClick={roll}>Roll Dice</button>
         <div>{die}</div>
+        <div>{die2}</div>
+        <div>{die3}</div>
     </div>
     );
 }
--- client/src/screens/Game.jsx
+++ client/src/screens/Game.jsx
@@ -5,7 +5,10 @@ export default function Game() {
   return (
     <div className="flex flex-col items-center">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Risk_game_board.svg/600px-Risk_game_board.svg.png" alt="risk" />
-        <RollDice/>
+        <div className='flex p-5'>
+          <RollDice/>
+          <RollDice/>
+        </div>
     </div>
   );
 }
