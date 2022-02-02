5a6994b4a5d86a6bea884885f5a5543bcc79faa3
Kevin Cleary <kjc2274@yahoo.com>
2022-02-02 11:07:39 -0500

    made signup form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Signup from './screens/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
     </div>
   );
 }
