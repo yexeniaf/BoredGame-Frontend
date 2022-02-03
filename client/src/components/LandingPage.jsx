24b4d5d30d9233cf36e5b538c5c302670a381e79
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-02 22:33:40 -0500

    styling and save button skeleton

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,35 +1,36 @@
+import { Link } from "react-router-dom"
+
 export default function LandingPage() {
     return(
-        <div class="relative z-10 pb-8 bg-black sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-            <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-                <rectangle/>
-            </svg>       
-            <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-                <div class="sm:text-center lg:text-left">
-                    <h1 class="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
-                        <div class="block xl">Welcome</div>
+        <div id="landing-page">
+        <div className="bg-black lg:w-full lg:pb-28 xl:pb-32">     
+            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                <div className="sm:text-center lg:text-left">
+                    <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
+                        <div className="block xl">Welcome</div>
                         <br/>
-                        <div class="block text-indigo-600 xl">Bored? Game!</div>
+                        <div className="block text-indigo-600 xl">Bored? Game!</div>
                     </h1>
-                    <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                        <div class="rounded-md shadow">
-                            <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                    <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                        <div className="rounded-md shadow">
+                            <Link to="/risk" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
                                 Play as Guest
-                            </a>
+                            </Link>
                         </div>
-                        <div class="mt-3 sm:mt-0 sm:ml-3">
-                            <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                        <div className="mt-3 sm:mt-0 sm:ml-3">
+                            <Link to="/signup" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
                                 Sign Up 
-                             </a>
+                             </Link>
                         </div>
-                        <div class="mt-3 sm:mt-0 sm:ml-3">
-                            <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                        <div className="mt-3 sm:mt-0 sm:ml-3">
+                            <Link to="/login" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
                                 Login 
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
             </main>
+        </div>
         </div>  
     )
 }
