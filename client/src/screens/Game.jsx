541c25f64601b8eab1e72047d01144dabb9c50bc
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-04 16:12:25 -0500

    Conditionally renders select number of players.

--- client/src/screens/Game.jsx
+++ client/src/screens/Game.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from '../components/Counter';
 import Combat from '../components/Dice/Combat';
 import RollDice from '../components/Dice/RollDice';
 import PlayerCard from '../components/PlayerCard';
+import PlayerNumSelect from '../components/PlayerNumSelect';
 import Setup from '../components/Setup';
 
 export default function Game() {
-  return (
-    <div className="flex flex-col items-center">
-        <img className='w-screen' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Risk_game_board.svg/600px-Risk_game_board.svg.png" alt="risk" />
-        <div className="counters">
-          <div>
-            <h2>Player 1 </h2>
-            <Counter />
+  const [toggle, setToggle] = useState(false);
+  const [playerNum, setPlayerNum] = useState({ playerNum: 2 });
+
+  if (toggle) {
+    console.log("playerNum:", playerNum);
+    return (
+      <div className="flex flex-col items-center">
+          <img className='w-screen' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Risk_game_board.svg/600px-Risk_game_board.svg.png" alt="risk" />
+          <div className="counters">
+            <div>
+              <h2>Player 1 </h2>
+              <Counter />
+            </div>
+            <div>
+              <h2>Player 2 </h2>
+              <Counter />
+            </div>
           </div>
-          <div>
-            <h2>Player 2 </h2>
-            <Counter />
+          <div className='flex p-5'>
+            <RollDice/>
+            <Combat troopNum1={3} troopNum2={2}/>
+            <Setup/>
           </div>
-        </div>
-        <div className='flex p-5'>
-          <RollDice/>
-          <Combat troopNum1={3} troopNum2={2}/>
-          <Setup/>
-        </div>
-        <div className='absolute bg-red-800 h-screen right-0'>
-          <PlayerCard num={1} num2={25}/>
-        </div>
-    </div>
-  );
+          <div className='absolute bg-red-800 h-screen right-0'>
+            <PlayerCard num={1} num2={25}/>
+          </div>
+      </div>
+    );  
+  } else {
+    return (
+      <PlayerNumSelect
+        setToggle = {setToggle}
+        setPlayerNum = {setPlayerNum}
+      />
+    )
+  }
 }
