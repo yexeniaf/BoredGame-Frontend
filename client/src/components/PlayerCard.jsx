51210e0c855b4ef4977a8b729bebe6ea51e057b9
Kevin Cleary <kjc2274@yahoo.com>
2022-02-05 12:06:05 -0500

    made dice rolling div

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -5,8 +5,8 @@ export default function PlayerCard(props) {
 
   return (
     <div className='bg-orange-400 text-amber-800 w-60 flex p-2 m-3 flex-col items-start'>
-        <h1 className='text-2xl'>Player {num}</h1>
-        <div>Troops to Deploy: {num2}</div>
+        <h1 className='text-2xl font-serif font-bold'>Player {num}</h1>
+        <div className='font-serif'>Troops to Deploy: {num2}</div>
     </div>
   );
 }
--- client/src/screens/Game.jsx
+++ client/src/screens/Game.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Counter from '../components/Counter';
 import Combat from '../components/Dice/Combat';
+import RollDice from '../components/Dice/RollDice';
+import RollOneDie from '../components/Dice/RollOneDie';
+import RollTwoDice from '../components/Dice/RollTwoDice';
 import PlayerCard from '../components/PlayerCard';
 import PlayerNumSelect from '../components/PlayerNumSelect';
 import Setup from '../components/Setup';
@@ -12,10 +15,10 @@ export default function Game() {
   const [troopNum2, setTroopNum2] = useState(6);
 
   if (toggle) {
-    console.log("playerNum:", playerNum);
+    // console.log("playerNum:", playerNum);
     return (
       <div className="flex flex-col items-center">
-          <img className='w-screen' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Risk_game_board.svg/600px-Risk_game_board.svg.png" alt="risk" />
+          <img className='w-3/4 left-0 absolute' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Risk_game_board.svg/600px-Risk_game_board.svg.png" alt="risk" />
           <div className="counters">
             <div>
               <h2>Player 1 </h2>
@@ -26,17 +29,22 @@ export default function Game() {
               <Counter />
             </div>
           </div>
-          <div className='flex p-5'>
+          {/* <div className='flex p-5'>
             <Combat 
             troopNum1={troopNum1} 
             troopNum2={troopNum2}
             setTroopNum1 ={setTroopNum1}
             setTroopNum2={setTroopNum2}
             />
+          </div> */}
+          <div className='fixed bottom-0 right-72 flex p-5 bg-red-900 border-2 border-x-amber-500 rounded-xl'>
+            <RollDice/>
+            <RollTwoDice/>
+            <RollOneDie/>
             <Setup/>
           </div>
-          <div className='absolute bg-red-800 h-screen right-0'>
-            <PlayerCard num={1} num2={25}/>
+          <div className='absolute bg-red-900 h-full right-0 border-2 border-x-amber-500'>
+            <PlayerCard num={playerNum.playerNum} num2={25}/>
           </div>
       </div>
     );  
