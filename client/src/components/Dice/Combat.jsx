879a331e3e6b7e034330c3b245b8e4f0287bcbec
Kevin Cleary <kjc2274@yahoo.com>
2022-02-04 21:32:03 -0500

    made some progress with combat function

--- client/src/components/Dice/Combat.jsx
+++ client/src/components/Dice/Combat.jsx
@@ -1,33 +1,94 @@
+import { useEffect, useRef, useState } from "react";
 import DieRoll from "./DieRoll.js"
-
-//taken from stack-overflow
-function repeat(func, times) {
-    func();
-    times && --times && repeat(func, times);
-}
 
 
 export default function Combat(props) {
-    const {troopNum1, troopNum2} = props
+    const [toggle, setToggle] = useState(false)
+    const {troopNum1, troopNum2, setTroopNum1, setTroopNum2} = props
+    const initialRender = useRef(true);
     //store the values rolled by both players for comparison
-    const att = [];
-    const def = [];
- 
+    const [att, setAtt] = useState([]);
+    const [def, setDef] = useState([]);
+    let life1 = troopNum1;
+    let life2 = troopNum2;
 
-    //set up if else statements to determine how many dice should be rolled for each player
-    if(troopNum1 > 2 && troopNum2 > 1){
-        repeat(()=> att.push(DieRoll()), 3);
-        repeat(()=> def.push(DieRoll()), 2)
+    const handleClick = ()=>{
+        setToggle((prevToggle)=> !prevToggle)
     }
-    console.log(att);
+
+    useEffect(()=>{
+        if (initialRender.current) {
+            initialRender.current = false;
+          } else {
+                 //set up if else statements to determine how many dice should be rolled for each player
+       if(troopNum1 > 3 && troopNum2 > 1){
+        setAtt([DieRoll(), DieRoll(), DieRoll()]);
+        setDef([DieRoll(), DieRoll()])
+        }else if(troopNum1 === 3 && troopNum2 > 1){
+            setAtt([DieRoll(), DieRoll()]);
+            setDef([DieRoll(), DieRoll()])
+        }else if(troopNum1 === 2 && troopNum2 > 1){
+            setAtt([DieRoll()]);
+            setDef([DieRoll(), DieRoll()])
+        }else if(troopNum1 > 3 && troopNum2 === 1){
+            setAtt([DieRoll(), DieRoll(), DieRoll()]);
+            setDef([DieRoll()]);
+        }else if(troopNum1 === 3 && troopNum2 === 1){
+            setAtt([DieRoll(), DieRoll()]);
+            setDef([DieRoll()]);
+        }else if(troopNum1 === 2 && troopNum2 === 1){
+            setAtt([DieRoll()]);
+            setDef([DieRoll()]);
+        }
+        att.sort((a,b)=> b-a);
+        def.sort((a,b)=> b-a);
+        }
+        console.log(att, def);
+
+        if(att.length >= def.length){
+            console.log(att, def);
+            def.map((e, i)=>{
+                if(e >= att[i]){
+                    setTroopNum1(life1 -= 1);
+                }else{
+                    setTroopNum2(life2 -= 1);
+                }
+            })
+        }else{
+            if(att[0] <= def[0]){
+               setTroopNum1(life1 - 1);
+            }else{
+                setTroopNum2(life2 - 1);
+            }
+        }
+        console.log(troopNum1, troopNum2);
+    }, [toggle])
+    
 
     return (
         <div>
-            <div>
-                {att}
-            </div>
-            <div>
-                {def}
+            <button onClick={()=> handleClick()}>Fight!</button>
+            <div className="bg-gray-400 w-64 grid grid-cols-2 grid-rows-2 justify-items-center">
+                <div className="m-2">
+                    {att.map((e, i)=>{
+                        return(
+                            <div className="border-2 border-solid border-black w-8 h-8 rounded-md bg-white" key={i}>
+                                {e}
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="m-2">
+                {def.map((e, i)=>{
+                    return(
+                        <div className="border-2 border-solid border-black w-8 h-8 rounded-md bg-white" key={i}>
+                            {e}
+                        </div>
+                    )
+                })}
+                </div>
+            <div className="w-20">Attacking troops:{troopNum1}</div>
+            <div className="w-20">Defending troops:{troopNum2}</div>
             </div>
         </div>
     );
--- client/src/screens/Game.jsx
+++ client/src/screens/Game.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import{ useState, useEffect } from 'react';
 import Counter from '../components/Counter';
 import Combat from '../components/Dice/Combat';
 import RollDice from '../components/Dice/RollDice';
@@ -6,6 +6,10 @@ import PlayerCard from '../components/PlayerCard';
 import PlayerNumSelect from '../components/PlayerNumSelect';
 
 export default function Game() {
+  const [troopNum1, setTroopNum1] = useState(8);
+  const [troopNum2, setTroopNum2] = useState(6);
+
+
   return (
     <div className="flex flex-col items-center">
         <img className='w-screen' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Risk_game_board.svg/600px-Risk_game_board.svg.png" alt="risk" />
@@ -20,8 +24,12 @@ export default function Game() {
           </div>
         </div>
         <div className='flex p-5'>
-          <RollDice/>
-          <Combat troopNum1={3} troopNum2={2}/>
+          <Combat 
+          troopNum1={troopNum1} 
+          troopNum2={troopNum2}
+          setTroopNum1 ={setTroopNum1}
+          setTroopNum2={setTroopNum2}
+          />
         </div>
         <div className='absolute bg-red-800 h-screen right-0'>
           <PlayerCard num={1} num2={25}/>
