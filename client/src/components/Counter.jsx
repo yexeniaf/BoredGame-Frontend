16f5e1e1024eceeb972c33fce87c4efdf3975869
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-02-05 23:00:15 -0500

    format

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -1,30 +1,34 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 export default function Counter(props) {
-    const [count, setCount] = useState(props.troops);
-    
-    let increaseNum =()=>{
-    if(count<180)
-    {
-    setCount(Number(count)+1);
+  const [count, setCount] = useState(props.troops);
+
+  let increaseNum = () => {
+    if (count < 180) {
+      setCount(Number(count) + 1);
     }
   };
   let decreaseNum = () => {
-     if(count>0)
-     {
+    if (count > 0) {
       setCount(count - 1);
-     }
-  }
- let handleChange = (e)=>{
-   setCount(e.target.value);
-  }
+    }
+  };
+  let handleChange = (e) => {
+    setCount(e.target.value);
+  };
 
-  return( 
+  return (
     <div className="counter">
-        <h1 className='font-bold'>{count}</h1>
-        <button className='mx-1 text-white bg-red-600 w-6' onClick={decreaseNum}>-</button>
-        <button className='mx-1 text-white bg-green-500 w-6' onClick={increaseNum}>+</button>
+      <h1 className="font-bold">{count}</h1>
+      <button className="mx-1 text-white bg-red-600 w-6" onClick={decreaseNum}>
+        -
+      </button>
+      <button
+        className="mx-1 text-white bg-green-500 w-6"
+        onClick={increaseNum}
+      >
+        +
+      </button>
     </div>
-
-  )
+  );
 }
