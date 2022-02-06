fe926eebb6409c5ef0b5bba2f83b64829690ce93
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-02-05 21:25:07 -0500

    formatted table.jsx

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,58 +1,66 @@
-import { useState } from 'react';
-import Counter from './Counter';
-import data from "../mock-data.json"
-
+import { useState } from "react";
+import Counter from "./Counter";
+import data from "../mock-data.json";
 
 export default function Table() {
-    const [values, setValue] = useState(data)
-    const [show, setShow] = useState(true)
+  const [values, setValue] = useState(data);
+  const [show, setShow] = useState(true);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
 
-
-  return(
+  return (
     <div>
-        <h1 className='text-lg font-bold text-yellow-50'>LeaderBoard</h1>
-        <button className="w-16 text-sm font-bold bg-amber-400 border-1 border-black rounded-sm m-2" onClick={()=> setShow(true)}>Open</button>
-        <button className="w-16 text-sm font-bold bg-amber-400 border-1 border-black rounded-sm m-2" onClick={()=> setShow(false)}>Close</button>
-        { show? 
-            <div className='results'>
-                <div className='table'>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th className='text-yellow-50'>Territory</th>
-                                <th className='text-yellow-50'>Troops</th>
-                                <th className='text-yellow-50'>Owner</th>
-                            </tr>
-                            {values.map((value) => {
-                                return (
-                                    <tr>
-                                        <td>{value.territory}</td>
-                                        <td>
-                                            {value.troops}
-                                            <Counter troops={0}/>
-                                        </td>
-                                        <td>
-                                            {value.owner}
-                                            <form onSubmit={handleSubmit}>
-                                                <input
-                                                    className='owners'
-                                                    type="text"
-                                                    placeholder='Territory Owner'
-                                                />
-                                            </form>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        :null}
+      <h1 className="text-lg font-bold text-yellow-50">LeaderBoard</h1>
+      <button
+        className="w-16 text-sm font-bold bg-amber-400 border-1 border-black rounded-sm m-2"
+        onClick={() => setShow(true)}
+      >
+        Open
+      </button>
+      <button
+        className="w-16 text-sm font-bold bg-amber-400 border-1 border-black rounded-sm m-2"
+        onClick={() => setShow(false)}
+      >
+        Close
+      </button>
+      {show ? (
+        <div className="results">
+          <div className="table">
+            <table>
+              <tbody>
+                <tr>
+                  <th className="text-yellow-50">Territory</th>
+                  <th className="text-yellow-50">Troops</th>
+                  <th className="text-yellow-50">Owner</th>
+                </tr>
+                {values.map((value) => {
+                  return (
+                    <tr>
+                      <td>{value.territory}</td>
+                      <td>
+                        {value.troops}
+                        <Counter troops={0} />
+                      </td>
+                      <td>
+                        {value.owner}
+                        <form onSubmit={handleSubmit}>
+                          <input
+                            className="owners"
+                            type="text"
+                            placeholder="Territory Owner"
+                          />
+                        </form>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : null}
     </div>
-  )
+  );
 }
