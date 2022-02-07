fd9d05d1f41a87e40efece944db00db148a35f54
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-07 14:37:14 -0500

    Passes territories to saved game button as prop. Passes territories, and setTerritories to table as prop.

--- client/src/screens/Game.jsx
+++ client/src/screens/Game.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
+import defaultTerritories from "../json/new-territory-data.json"
 import RollDice from '../components/Dice/RollDice';
 import RollOneDie from '../components/Dice/RollOneDie';
 import RollTwoDice from '../components/Dice/RollTwoDice';
@@ -11,13 +12,6 @@ import Setup from '../components/Setup';
 import Table from '../components/Table';
 import Turn from '../scripts/Turn.js';
 
-const dummy_territory = {
-  territory: "Alaska",
-  cardOwner: 2,
-  troops: 3,
-  territoryOwner: 1
-}
-
 export default function Game() {
   // This opens or collapses the player stats board.
   const [show, setShow] = useState(true)
@@ -25,15 +19,15 @@ export default function Game() {
   // Toggle to control display of player number selector and main game.
   const [toggle, setToggle] = useState(false);
 
-  // Toggle to Refresh game state when you save.
-  // const [toggleSave, setToggleSave] = useState(false);
-
   const initialRender = useRef(true);
-  // const [troopNum1, setTroopNum1] = useState(8);
-  // const [troopNum2, setTroopNum2] = useState(6);
 
   // Number of players:
   const [playerNum, setPlayerNum] = useState(2);
+
+  // Sets initial state for territories. 
+  const [territories, setTerritories] = useState(defaultTerritories)
+
+  const [currentTurn, setCurrentTurn] = useState(1);
 
   // An array with player card elements eqaul to the number of players:
   const [players, setPlayers] = useState([]);
@@ -41,8 +35,6 @@ export default function Game() {
   // The starting troop allottment per player:
   const [startingTroops, setStartingTroops] = useState(0);
 
-  const [currentTurn, setCurrentTurn] = useState(1);
-  
   // When the toggle is fired, make an array of playyers.
   useEffect(() => {
     if (initialRender.current) {
@@ -57,7 +49,7 @@ export default function Game() {
     try {
       let gameState = {
           playerNum: playerNum,
-          territories: dummy_territory,
+          territories: territories,
           turn: currentTurn
       }
       const res = await axios.post(`https://boredgame-backend.herokuapp.com/gamestate`, gameState)
@@ -97,19 +89,13 @@ export default function Game() {
         <div className='absolute bg-red-900 border-2 border-x-amber-500 left-0'>
               <Table
                 playerNum = {playerNum}
+                territories = {territories}
+                setTerritories = {setTerritories}
               />
             </div>
           <div className="counters">
             
           </div>
-          {/* <div className='flex p-5'>
-            <Combat 
-            troopNum1={troopNum1} 
-            troopNum2={troopNum2}
-            setTroopNum1 ={setTroopNum1}
-            setTroopNum2={setTroopNum2}
-            />
-          </div> */}
           <div className='fixed bottom-0 right-72 flex p-5 bg-red-900 border-2 border-x-amber-500 rounded-xl'>
             <RollDice/>
             <RollTwoDice/>
@@ -121,27 +107,27 @@ export default function Game() {
             <button className="w-16 text-sm font-bold bg-amber-400 border-1 border-black rounded-sm m-2" onClick={()=> setShow(true)}>Open</button>
             <button className="w-16 text-sm font-bold bg-amber-400 border-1 border-black rounded-sm m-2" onClick={()=> setShow(false)}>Close</button>
           { show? 
-           <div>
-            {players}
-            <h4 className='text-lg font-bold text-yellow-50'>It is player {currentTurn}'s turn!</h4>
-            <button
-              className="w-full flex items-center justify-center px-8 py-3 border
-              border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700
-              md:py-4 md:text-lg md:px-10"
-              onClick={handleTurnPass}
-            >
-              Next Turn
-            </button>
-            <br/>
-            {/* Currently the save button doesn't actually pass the territory information. That will need to be saved in state and passed down. */}
-            <SaveGameButton
-              playerNum = {playerNum}
-              territories = {"territories"}
-              turn = {currentTurn}
-              saveGame = {saveGame}
-            />
-          </div>
- :null}
+            <div>
+              {players}
+              <h4 className='text-lg font-bold text-yellow-50'>It is player {currentTurn}'s turn!</h4>
+              <button
+                className="w-full flex items-center justify-center px-8 py-3 border
+                border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700
+                md:py-4 md:text-lg md:px-10"
+                onClick={handleTurnPass}
+              >
+                Next Turn
+              </button>
+              <br/>
+              {/* Currently the save button doesn't actually pass the territory information. That will need to be saved in state and passed down. */}
+              <SaveGameButton
+                playerNum = {playerNum}
+                territories = {territories}
+                turn = {currentTurn}
+                saveGame = {saveGame}
+              />
+            </div>
+            :null}
       </div>
       </div>
     );  
@@ -156,6 +142,3 @@ export default function Game() {
     )
   }
 }
-
-
-
