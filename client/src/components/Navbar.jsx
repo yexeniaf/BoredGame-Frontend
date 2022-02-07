a1ea174345c0675007b68301b1bfef18b7fd87a5
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-07 15:39:11 -0500

    logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom"
 export default function Navbar(props) {
     return (
         <nav className="nav text-lg px-4 py-4 bg-stone-900">
-            <Link to="/">
-                <h1 className='font-extrabold text-stone-300'>Bored? Game!</h1>
+            <Link className='brand' to="/">
+                <img className='logo' src="https://www.seekpng.com/png/full/70-707290_svg-black-and-white-arcade-clipart-arcade-machine.png" alt="arcade game"/>
+                <h1 className='font-extrabold text-2xl text-stone-300'>Bored? Game!</h1>
             </Link>
-            <ul className="links text-stone-300">
+            <ul className="links font-semibold text-stone-300">
                 <li>
                     <Link to="/saved">Saved</Link>
                 </li>
