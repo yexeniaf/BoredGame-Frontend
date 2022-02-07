f58d3e8688243594507119af8826fa510b4d4adf
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-07 15:54:14 -0500

    navbar minor styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar(props) {
         <nav className="nav text-lg px-4 py-4 bg-black">
             <Link className='brand' to="/">
                 <img className='logo' src="https://www.seekpng.com/png/full/70-707290_svg-black-and-white-arcade-clipart-arcade-machine.png" alt="arcade game"/>
-                <h1 className='font-extrabold text-2xl text-stone-300'>Bored? Game!</h1>
+                <h1 className='font-extrabold text-2xl text-stone-300 shadow-lg shadow-indigo-500/50'>Bored? Game!</h1>
             </Link>
             <ul className="links font-semibold text-stone-300">
                 <li>
