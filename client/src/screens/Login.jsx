aa8f144b8023ef5629d62e8d414ed1389f034e94
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2022-02-07 18:07:15 -0500

    login styling

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -37,9 +37,9 @@ export default function Login() {
     }
 
     return (
-      <div className="signin flex flex-col items-center">
-          <form onSubmit={handleSubmit} className="flex flex-col items-center bg-gray-300 m-5 p-10 w-90">
-          <img className='logo' src="https://www.seekpng.com/png/full/70-707290_svg-black-and-white-arcade-clipart-arcade-machine.png" alt="arcade game"/>
+      <div className=" flex flex-col items-center">
+          <form onSubmit={handleSubmit} className="signin flex flex-col items-center text-white text-xlg font-semibold bg-stone-700 m-5 p-20 w-90">
+          <img className='logo2' src="https://www.seekpng.com/png/full/70-707290_svg-black-and-white-arcade-clipart-arcade-machine.png" alt="arcade game"/>
           <h2 id="login">Login</h2>
           <br/>
               <label htmlFor="email">E-mail:</label>
