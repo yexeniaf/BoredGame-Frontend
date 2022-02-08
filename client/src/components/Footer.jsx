9534cb1f1882edb17af52c252ab189dd37484e00
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-02-07 22:47:47 -0500

    removed h7 tag from footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,54 +1,92 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
-  return  (
+  return (
     <footer className="fixed inset-x-0 bottom-0 w-full bg-stone-800">
-        <div className='social-media'>
-            <div className="items-center text-white">
-                <h7>NICK MILLER</h7>
-                <div className='icons'>
-                    <a href="https://github.com/nickdbmiller" target="_blank">
-                        <img className='h-7 w-7 rounded'  src='https://cdn3.iconfinder.com/data/icons/inficons/512/github.png' alt="github icon"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/ndbmiller/" target="_blank">
-                        <img className="h-7 w-7 rounded" src='https://cdn-icons-png.flaticon.com/512/174/174857.png' alt="LinkedIn icon"/>
-                    </a>
-                </div>
-            </div>
-            <div className="items-center text-white">
-                <h5>MAYA HEILPERN</h5>
-                <div className='icons'>
-                    <a href="https://github.com/mayaheilpern" target="_blank">
-                        <img className='h-7 w-7 rounded'  src='https://cdn3.iconfinder.com/data/icons/inficons/512/github.png' alt="github icon"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/mayaheilpern/" target="_blank">
-                        <img className="h-7 w-7 rounded" src='https://cdn-icons-png.flaticon.com/512/174/174857.png' alt="LinkedIn icon"/>
-                    </a>
-                </div>
-            </div>
-            <div className=" items-center text-white">
-                <h5>KEVIN CLEARY</h5>
-                <div className='icons'>
-                    <a href="https://github.com/kjc2274" target="_blank">
-                        <img className='h-7 w-7 rounded'  src='https://cdn3.iconfinder.com/data/icons/inficons/512/github.png' alt="github icon"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/kevin-cleary-374a65228/" target="_blank">
-                        <img className="h-7 w-7 rounded" src='https://cdn-icons-png.flaticon.com/512/174/174857.png' alt="LinkedIn icon"/>
-                    </a>
-                </div>
-            </div>
-            <div className=" items-center text-white">
-                <h5>YEXENIA FLORES</h5>
-                <div className='icons'>
-                    <a href="https://github.com/yexeniaf" target="_blank">
-                        <img className='h-7 w-7 rounded'  src='https://cdn3.iconfinder.com/data/icons/inficons/512/github.png' alt="github icon"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/yexenia-flores/" target="_blank">
-                        <img className="h-7 w-7 rounded" src='https://cdn-icons-png.flaticon.com/512/174/174857.png' alt="LinkedIn icon"/>
-                    </a>
-                </div>
-            </div>                      
+      <div className="social-media">
+        <div className="items-center text-white">
+          <h5>NICK MILLER</h5>
+          <div className="icons">
+            <a href="https://github.com/nickdbmiller" target="_blank">
+              <img
+                className="h-7 w-7 rounded"
+                src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
+                alt="github icon"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/ndbmiller/" target="_blank">
+              <img
+                className="h-7 w-7 rounded"
+                src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+                alt="LinkedIn icon"
+              />
+            </a>
+          </div>
         </div>
+        <div className="items-center text-white">
+          <h5>MAYA HEILPERN</h5>
+          <div className="icons">
+            <a href="https://github.com/mayaheilpern" target="_blank">
+              <img
+                className="h-7 w-7 rounded"
+                src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
+                alt="github icon"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/mayaheilpern/" target="_blank">
+              <img
+                className="h-7 w-7 rounded"
+                src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+                alt="LinkedIn icon"
+              />
+            </a>
+          </div>
+        </div>
+        <div className=" items-center text-white">
+          <h5>KEVIN CLEARY</h5>
+          <div className="icons">
+            <a href="https://github.com/kjc2274" target="_blank">
+              <img
+                className="h-7 w-7 rounded"
+                src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
+                alt="github icon"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kevin-cleary-374a65228/"
+              target="_blank"
+            >
+              <img
+                className="h-7 w-7 rounded"
+                src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+                alt="LinkedIn icon"
+              />
+            </a>
+          </div>
+        </div>
+        <div className=" items-center text-white">
+          <h5>YEXENIA FLORES</h5>
+          <div className="icons">
+            <a href="https://github.com/yexeniaf" target="_blank">
+              <img
+                className="h-7 w-7 rounded"
+                src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
+                alt="github icon"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/yexenia-flores/"
+              target="_blank"
+            >
+              <img
+                className="h-7 w-7 rounded"
+                src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+                alt="LinkedIn icon"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
-  )
+  );
 }
